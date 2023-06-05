fc339f3ec291c3aedf52c473d0bef6336864ac71
seboksin <mk@cblab.ru>
2023-06-05 19:13:14 +0700

    test

--- Sources/FancyWebRTC/FancyRTCMediaDevices.swift
+++ Sources/FancyWebRTC/FancyRTCMediaDevices.swift
@@ -217,14 +217,18 @@ public class FancyRTCMediaDevices: NSObject {
     @available(iOS 11.0, *)
     static func doStartRecording(recorder: RPScreenRecorder, factory: RTCPeerConnectionFactory, localStream: RTCMediaStream, listener: @escaping (_ stream : FancyRTCMediaStream?, _ error : String?) -> ()) {
         listener(nil,"-----------doStartRecording")
+         listener(nil,"-----------doStartRecording1")
          let recorder = RPScreenRecorder.shared()
                 if (recorder.isRecording) {
+                 listener(nil,"-----------doStartRecording2")
                     recorder.stopCapture { (error) in
                         if(error != nil){
                             print(error!.localizedDescription)
                         }
                     }
                 }
+                 listener(nil,"-----------doStartRecording3")
+
 //         if (recorder.isAvailable) {
 //             let videoSource = factory.videoSource()
 //             let videoTrack = factory.videoTrack(with: videoSource, trackId: UUID().uuidString)
