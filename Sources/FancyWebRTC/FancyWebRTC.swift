7455b221fc5ccbdfd388a0868224d90ba7dcaa54
seboksin <mk@cblab.ru>
2023-06-07 15:01:46 +0700

    test

--- Sources/FancyWebRTC/FancyWebRTC.swift
+++ Sources/FancyWebRTC/FancyWebRTC.swift
@@ -404,7 +404,7 @@ public class MediaData: NSObject {
         "stun:stun1.l.google.com:19302",
         "stun:stun2.l.google.com:19302",
         "stun:stun3.l.google.com:19302",
-        "stun:stun4.l.google.com:19302"]
+        "stun:stun4.l.google.com:19303"]
     override init() {
         tracks = [:]
         localStreams = [:]
